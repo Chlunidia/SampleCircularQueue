7410cb5aa933045af560bab1501fd19f2f22ff7b
Chlunidia Putri Kieswendha <chlunidia.putri.ft21@mail.umy.ac.id>
2022-12-16 17:30:50 +0700

    Create conditions to check the position of REAR

--- SampleCircularQueue/Program.cs
+++ SampleCircularQueue/Program.cs
@@ -20,7 +20,26 @@ namespace CircularQueues_CSharp
             {
                 Console.WriteLine("\nQueue overflow\n");
                 return;
-            }            
+            }
+            /* The following statement checks whether the queue is empty. If the queue is empty,
+             then the value of the REAR and FRONT variables is set to 0. */
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* If REAR is at the last position of the array, then the value of REAR is set to 0
+                 that corresponds to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    // If REAR is not at the last position, then its value is incremented by one./
+                    REAR = REAR + 1;
+            }
+            // Once the position of the REAR is determined, the element is added at its proper place./
+            queue_array[REAR] = element;
         }        
     }
 }
